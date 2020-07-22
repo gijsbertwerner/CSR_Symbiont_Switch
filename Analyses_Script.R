fc4f5a61bd9322012b466027120905d4afb333c2
gijsbertwerner <gijsbert.werner@zoo.ox.ac.uk>
2020-07-22 17:05:52 +0200

    Basic data cleaning and tree selection.

--- Analyses_Script.R
+++ Analyses_Script.R
@@ -16,9 +16,41 @@ dat_CSR_symb <-
            strip.white = T)
 head(dat_CSR_symb)
 sapply(dat_CSR_symb, class)
+nrow(dat_CSR_symb)
 
-zanne_tree<-read.tree("./Data/Vascular_Plants_rooted.dated.tre")
+zanne_tree <- read.tree("./Data/Vascular_Plants_rooted.dated.tre")
 zanne_tree
 
-smith_brown_tree<-read.tree("./Data/ALLMB.tre")
+smith_brown_tree <- read.tree("./Data/ALLMB.tre")
 smith_brown_tree
+
+# Data Cleaning -----------------------------------------------------------
+
+#Species formatting
+head(dat_CSR_symb$Species_name)
+head(zanne_tree$tip.label)
+head(smith_brown_tree$tip.label)
+dat_CSR_symb$Species_name <- gsub(pattern = " ",
+                                  replacement = "_",
+                                  dat_CSR_symb$Species_name)
+
+#How many of the 3014 species in the database are absent in Zanne?
+length(setdiff(dat_CSR_symb$Species_name, zanne_tree$tip.label))
+
+#How many of the 3014 species in the database are absent in Smith and Brown?
+length(setdiff(dat_CSR_symb$Species_name, smith_brown_tree$tip.label))
+#Only 320 of 3014 lacking. I.e. ~90% is present. That seems good enough for now.
+write.csv(
+  setdiff(dat_CSR_symb$Species_name, smith_brown_tree$tip.label),
+  quote = F,
+  row.names = F,
+  file = "./Data/Cosme_Species_Missing_Smith_Tree.csv"
+)
+
+#Extract the appropriate subtree from Smith&Brown
+analysis_tree <-
+  drop.tip(
+    phy = smith_brown_tree,
+    tip = setdiff(smith_brown_tree$tip.label,dat_CSR_symb$Species_name)
+  )
+analysis_tree
