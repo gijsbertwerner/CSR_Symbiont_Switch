073af2166959803e8c9d863d7ffb3d91a37c178f
gijsbertwerner <gijsbert.werner@zoo.ox.ac.uk>
2020-07-22 16:48:19 +0200

    Read in data files and tree files

--- Analyses_Script.R
+++ Analyses_Script.R
@@ -1,7 +1,24 @@
 #Gijsbert Werner, University of Oxford
 #July 22, 2020
 
-#Loading Packages
-library(dplyr)
 
-#Loading data
+# Loading packages --------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+library(ape)
+
+# Loading data and trees --------------------------------------------------
+
+dat_CSR_symb <-
+  read.csv(file = "./Data/Plant species with symbiotic type and CSR strategy_Cosme et al 09-07-2020.csv",
+           as.is = T,
+           strip.white = T)
+head(dat_CSR_symb)
+sapply(dat_CSR_symb, class)
+
+zanne_tree<-read.tree("./Data/Vascular_Plants_rooted.dated.tre")
+zanne_tree
+
+smith_brown_tree<-read.tree("./Data/ALLMB.tre")
+smith_brown_tree
