04ea6bfb2e64847c8b689fc2b3c6182b0d5f0abe
gijsbertwerner <gijsbert.werner@zoo.ox.ac.uk>
2020-09-10 20:10:27 +0200

    Add correlated analysis

--- Analyses_Script.R
+++ Analyses_Script.R
@@ -84,12 +84,12 @@ analysis_dat_CSR_symb <-
 nrow(analysis_dat_CSR_symb)
 #More species in data set, than in tree.
 #This should not be possible. Could be due to duplicates in the dataset?
-analysis_dat_CSR_symb[duplicated(analysis_dat_CSR_symb$Species_name),]
+analysis_dat_CSR_symb[duplicated(analysis_dat_CSR_symb$Species_name), ]
 #Yes, some duplicates, but only about ~30. Small variations in CSR-values, agreement on symbiont type
 
 #For now, simply remove the duplicates. Other option, take average values for CSR values.
 analysis_dat_CSR_symb <-
-  analysis_dat_CSR_symb[!duplicated(analysis_dat_CSR_symb$Species_name), ]
+  analysis_dat_CSR_symb[!duplicated(analysis_dat_CSR_symb$Species_name),]
 nrow(analysis_dat_CSR_symb)
 #Now matches
 
@@ -182,26 +182,38 @@ table(analysis_dat_CSR_symb$Symbiotic_type) #States are numbered in the modeling
 
 #Consideration: we have lots of states (8), and some have only few cases.
 #We could simplify the inference by dropping some states and/or subsuming them in other states.
-#That would make inference easier, and the results more interpretable (not so many state transitions), but also lose biological detail. 
+#That would make inference easier, and the results more interpretable (not so many state transitions), but also lose biological detail.
 
 ##Let's for now plot this reconstruction onto the tree
 
 #Create a data frame to plot the trait data
-dat_plot_symbiont_type<-analysis_dat_CSR_symb_ASR_symbiont_type %>%
+dat_plot_symbiont_type <- analysis_dat_CSR_symb_ASR_symbiont_type %>%
   dplyr::select(Symbiotic_type)
-row.names(dat_plot_symbiont_type)<-analysis_dat_CSR_symb_ASR_symbiont_type$Species_name  
-dat_plot_symbiont_type$Symbiotic_type<-as.numeric(as.factor(dat_plot_symbiont_type$Symbiotic_type))
+row.names(dat_plot_symbiont_type) <-
+  analysis_dat_CSR_symb_ASR_symbiont_type$Species_name
+dat_plot_symbiont_type$Symbiotic_type <-
+  as.numeric(as.factor(dat_plot_symbiont_type$Symbiotic_type))
 head(dat_plot_symbiont_type)
 
 #Symbionts ASR
-pdf("./Output/ASRSymbiontType.pdf",width = 20,height = 20)
-trait.plot(tree = analysis_tree,dat = dat_plot_symbiont_type,
-           cols = list(Symbiotic_type=brewer.pal(n=8,"Set2")),
-           type="f",legend=T,w=1/40,edge.width =2,
-           cex.lab = 0.01,tip.color="white",
-           show.node.label=T)
+pdf("./Output/ASRSymbiontType.pdf",
+    width = 20,
+    height = 20)
+trait.plot(
+  tree = analysis_tree,
+  dat = dat_plot_symbiont_type,
+  cols = list(Symbiotic_type = brewer.pal(n = 8, "Set2")),
+  type = "f",
+  legend = T,
+  w = 1 / 40,
+  edge.width = 2,
+  cex.lab = 0.01,
+  tip.color = "white",
+  show.node.label = T
+)
 nodelabels(pie = ASR_symbiont_type_ARD_yang$states,
-           piecol = brewer.pal(n=8,"Set2"),cex=0.3)
+           piecol = brewer.pal(n = 8, "Set2"),
+           cex = 0.3)
 add.scale.bar()
 dev.off()
 
@@ -211,14 +223,33 @@ dev.off()
 #1. Turn it into a categorical variable: assign one of three categorical states based on what it is most selected for.
 #2. Treat as three distinct continuous variables, and repeat the analyses for each.
 #Neither of these captures perfectly what we are trying to measure, but they may do for our purposes. Let's have a look.
-#I'll try only 1 for now, because it seems simplest and most informative. 
+#I'll try only 1 for now, because it seems simplest and most informative.
 
-analysis_dat_CSR_symb$selection_type<-
-  apply(analysis_dat_CSR_symb %>% dplyr::select(C.selection,S.selection,R.selection),1,which.max)
-analysis_dat_CSR_symb$selection_type<-  gsub(pattern = "1",replacement = "C",x = analysis_dat_CSR_symb$selection_type)
-analysis_dat_CSR_symb$selection_type<-  gsub(pattern = "2",replacement = "S",x = analysis_dat_CSR_symb$selection_type)
-analysis_dat_CSR_symb$selection_type<-  gsub(pattern = "3",replacement = "R",x = analysis_dat_CSR_symb$selection_type)
-table(analysis_dat_CSR_symb$selection_type) #Pretty equal numbers of all three types. 
+analysis_dat_CSR_symb$selection_type <-
+  apply(
+    analysis_dat_CSR_symb %>% dplyr::select(C.selection, S.selection, R.selection),
+    1,
+    which.max
+  )
+analysis_dat_CSR_symb$selection_type <-
+  gsub(
+    pattern = "1",
+    replacement = "C",
+    x = analysis_dat_CSR_symb$selection_type
+  )
+analysis_dat_CSR_symb$selection_type <-
+  gsub(
+    pattern = "2",
+    replacement = "S",
+    x = analysis_dat_CSR_symb$selection_type
+  )
+analysis_dat_CSR_symb$selection_type <-
+  gsub(
+    pattern = "3",
+    replacement = "R",
+    x = analysis_dat_CSR_symb$selection_type
+  )
+table(analysis_dat_CSR_symb$selection_type) #Pretty equal numbers of all three types.
 
 #Data formatting. We need two columns, species and symbiont state.
 analysis_dat_CSR_symb_ASR_selection_type <-
@@ -276,25 +307,121 @@ table(analysis_dat_CSR_symb$selection_type) #States are numbered in the modeling
 
 #Consideration: we have lots of states (8), and some have only few cases.
 #We could simplify the inference by dropping some states and/or subsuming them in other states.
+#That would make inference easier, and the results more interpretable (not so many state transitions), but also lose biological detail.
+
+##Let's for now plot this reconstruction onto the tree
+
+#Create a data frame to plot the trait data
+dat_plot_selection_type <-
+  analysis_dat_CSR_symb_ASR_selection_type %>%
+  dplyr::select(selection_type)
+row.names(dat_plot_selection_type) <-
+  analysis_dat_CSR_symb_ASR_selection_type$Species_name
+dat_plot_selection_type$selection_type <-
+  as.numeric(as.factor(dat_plot_selection_type$selection_type))
+head(dat_plot_selection_type)
+
+#CSR ASR
+pdf("./Output/ASRCSRType.pdf",
+    width = 20,
+    height = 20)
+trait.plot(
+  tree = analysis_tree,
+  dat = dat_plot_selection_type,
+  cols = list(selection_type = brewer.pal(n = 3, "Accent")),
+  type = "f",
+  legend = T,
+  w = 1 / 40,
+  edge.width = 2,
+  cex.lab = 0.01,
+  tip.color = "white",
+  show.node.label = T
+)
+nodelabels(pie = ASR_selection_type_ER_yang$states,
+           piecol = brewer.pal(n = 3, "Accent"),
+           cex = 0.3)
+add.scale.bar()
+dev.off()
+
+######Correlated evolution between the two variables
+
+#Data formatting. We need three columns, species and symbiont state and selection type.
+analysis_dat_CSR_symb_ASR_symbiont_selection_type <-
+  analysis_dat_CSR_symb %>% dplyr::select(Species_name, Symbiotic_type, selection_type)
+head(analysis_dat_CSR_symb_ASR_symbiont_selection_type)
+
+#Run ASRs
+ASR_symbiont_selection_type_ER_yang <-
+  corHMM(
+    phy = analysis_tree,
+    data = analysis_dat_CSR_symb_ASR_symbiont_selection_type,
+    rate.cat = 1,
+    model = "ER",
+    node.states = "marginal",
+    root.p = "yang",
+    nstarts = 20,
+    n.cores = 6
+  )
+ASR_symbiont_selection_type_ARD_yang <-
+  corHMM(
+    phy = analysis_tree,
+    data = analysis_dat_CSR_symb_ASR_symbiont_selection_type,
+    rate.cat = 1,
+    model = "ARD",
+    node.states = "marginal",
+    root.p = "yang",
+    nstarts = 20,
+    n.cores = 6
+  )
+ASR_symbiont_selection_type_SYM_yang <-
+  corHMM(
+    phy = analysis_tree,
+    data = analysis_dat_CSR_symb_ASR_symbiont_selection_type,
+    rate.cat = 1,
+    model = "SYM",
+    node.states = "marginal",
+    root.p = "yang",
+    nstarts = 20,
+    n.cores = 6
+  )
+
+#Which is the best model, using AIC-criteria?
+akaike.weights(
+  c(
+    ASR_symbiont_selection_type_ER_yang$AICc,
+    ASR_symbiont_selection_type_ARD_yang$AICc,
+    ASR_symbiont_selection_type_SYM_yang$AICc
+  )
+)
+
+#ARD by far the best
+ASR_symbiont_selection_type_ARD_yang
+plotMKmodel(ASR_symbiont_selection_type_ARD_yang)
+table(analysis_dat_CSR_symb$symbiont_selection_type) #States are numbered in the modeling: this is what types the numbers represent, they are ordered aphabetically, it sems.
+
+#Consideration: we have lots of states (8), and some have only few cases.
+#We could simplify the inference by dropping some states and/or subsuming them in other states.
 #That would make inference easier, and the results more interpretable (not so many state transitions), but also lose biological detail. 
 
 ##Let's for now plot this reconstruction onto the tree
 
 #Create a data frame to plot the trait data
-dat_plot_selection_type<-analysis_dat_CSR_symb_ASR_selection_type %>%
-  dplyr::select(selection_type)
-row.names(dat_plot_selection_type)<-analysis_dat_CSR_symb_ASR_selection_type$Species_name  
-dat_plot_selection_type$selection_type<-as.numeric(as.factor(dat_plot_selection_type$selection_type))
-head(dat_plot_selection_type)
+dat_plot_symbiont_selection_type<-analysis_dat_CSR_symb_ASR_symbiont_selection_type %>%
+  dplyr::select(Symbiotic_type,selection_type)
+row.names(dat_plot_symbiont_selection_type)<-analysis_dat_CSR_symb_ASR_symbiont_selection_type$Species_name  
+dat_plot_symbiont_selection_type$Symbiotic_type<-as.numeric(as.factor(dat_plot_symbiont_selection_type$Symbiotic_type))
+dat_plot_symbiont_selection_type$selection_type<-as.numeric(as.factor(dat_plot_symbiont_selection_type$selection_type))
+head(dat_plot_symbiont_selection_type)
 
 #CSR ASR
-pdf("./Output/ASRCSRType.pdf",width = 20,height = 20)
-trait.plot(tree = analysis_tree,dat = dat_plot_selection_type,
-           cols = list(selection_type=brewer.pal(n=8,"Set2")),
+pdf("./Output/ASRSymbiontCSRType.pdf",width = 20,height = 20)
+trait.plot(tree = analysis_tree,dat = dat_plot_symbiont_selection_type,
+           cols = list(Symbiotic_type=brewer.pal(n=8,"Set2"),
+                       selection_type=),
            type="f",legend=T,w=1/40,edge.width =2,
            cex.lab = 0.01,tip.color="white",
            show.node.label=T)
-nodelabels(pie = ASR_selection_type_ARD_yang$states,
+nodelabels(pie = ASR_symbiont_selection_type_ARD_yang$states,
            piecol = brewer.pal(n=8,"Set2"),cex=0.3)
 add.scale.bar()
 dev.off()
